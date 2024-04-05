4c46d6cd0dc1ffd81a42317c82abf28eb1933d9b
gabriel.adgeg <gad@octo.com>
2024-04-05 20:20:24 +0200

    BGSR

--- app/src/main/java/fr/octo/numresp/MainActivity.kt
+++ app/src/main/java/fr/octo/numresp/MainActivity.kt
@@ -12,14 +12,10 @@ import androidx.compose.runtime.rememberCoroutineScope
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.android.Android
 import io.ktor.client.plugins.cache.HttpCache
-import io.ktor.client.plugins.cache.storage.FileStorage
-import io.ktor.client.plugins.logging.ANDROID
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
 import kotlinx.coroutines.launch
-import java.nio.file.Files
-import java.nio.file.Paths
 
 class MainActivity : ComponentActivity() {
     private lateinit var httpClient: HttpClient
